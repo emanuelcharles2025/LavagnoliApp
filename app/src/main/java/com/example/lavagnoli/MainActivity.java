fb4ae726df7cbc9ea63691cb4c0871c46fbd9e0f
emanuelcharles2025 <emanuelcharlescrypto@gmail.com>
2025-07-04 09:33:46 -0300

    Commit inicial do projeto Lavagnoli

---
+++ app/src/main/java/com/example/lavagnoli/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.lavagnoli;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        WebView webView = new WebView(this);
+        setContentView(webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("http://10.1.1.218:93/?enderecoip=123");
+    }
+}
